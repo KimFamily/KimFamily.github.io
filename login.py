8af576ec7931f2ca4dea165791f78f27b829fdc2
jkim33 <jkim33@stuy.edu>
2019-07-31 00:33:54 -0400

    v1

---
+++ login.py
@@ -0,0 +1,32 @@
+print(
+    "<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>KimFamily | Sign Up</title>
+</head>
+<body>
+    <nav id="navbar">
+        <div class="container">
+            <h1 class="logo">
+                <span class="text-primary">Kim</span>Family
+            </h1>
+            <ul>
+                <li><a href="./login.html">Log In</a></li>
+                <li><a href="./signup.html">Sign Up</a></li>
+            </ul>
+        </div>
+    </nav>
+    <header id="showcase">
+        <div class="showcase-content">
+            <h1 class="l-heading">KimFamily Photos</h1>
+            <p class="lead">Memories that last forever.</p>
+        </div>
+    </header>
+    
+</body>
+</html>"
+)
---
+++ new_user.py
@@ -0,0 +1,57 @@
+import webbrowser
+new = 1
+
+original="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+scrambled="RWgUIa5v2TxsZj7otp3FqKrNEDzhL6B0O4QXHeicMYf1kSdGPubAVC8lnwy9mJ"
+
+def add(user, pw):
+    straw=open("users.csv", "a")
+    if (available(user)):
+        straw.write(encrypt(user) + "," + encrypt(pw) + "\n")
+        print("New Account Made.")
+        return True
+    else:
+        print("Username Already Taken.")
+        return False
+        
+        
+def available(user):
+    straw=open("users.csv", "rU")
+    data=straw.read()
+    data=data.split('\n')
+    data=data[:-1]
+    for i in range(len(data)):
+        data[i] = data[i].split(",")
+    user = encrypt(user)
+    for l in data:
+        if user == l[0]:
+            return False
+    return True
+
+def encrypt(text):
+    e = ""
+    for c in text:
+        if (original.find(c) == -1):
+            e += c
+        else:
+            e += scrambled[original.find(c)]
+    return e
+
+# def decrypt(text):
+#     d = ""
+#     for c in text:
+#         if (scrambled.find(c) == -1):
+#             d += c
+#         else:
+#             d += original[scrambled.find(c)]
+#     return d
+
+def main():
+    form=cgi.FieldStorage()
+    user = form.getValue("username",'')
+    pass1 = form.getValue("password1",'')
+    pass2 = form.getValue("password2",'')
+    if pass1 == pass2:
+        webbrowser.open("https://KimFamily.github.io/signup.html")
+
+main()
